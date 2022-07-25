37899e6593b1a3f73d04b5c16220f6d26d54494d
Federico NIGRO <federico.nigro93@gmail.com>
2022-07-25 18:10:24 +0200

    finishing the presentation for the Iterator Design pattern
    TODO: doing the DEMO standardization and proceding with the remaining patterns

---
+++ DesignPatterns/bin/Debug/DesignPatterns/Behavioural/Iterator/SongsOfThe80s.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.DesignPatterns.Behavioural.Iterator
+{
+  public class SongsOfThe80s
+  {
+    private SongInfo[] _bestSongs;
+
+    private int _maxNum = 3;
+
+    private int _currNum = 0;
+
+
+    
+    public SongsOfThe80s()
+    {
+      _bestSongs = new SongInfo[3];
+
+      AddSong("Roam", "B 52s", 1989);
+      AddSong("Cruel Summer", "Bananarama", 1984);
+      AddSong("Head Over Heels", "Tears For Fears", 1985);
+    }
+
+    public void AddSong(string songName, string bandName, int yearRelease)
+    {
+      SongInfo songInfo = new SongInfo(songName, bandName, yearRelease);
+
+      if(_currNum < _maxNum)
+        _bestSongs[_currNum] = songInfo;
+      // incremento su array di partenza 
+      else
+      {
+        SongInfo[] _newSongs = new SongInfo[_maxNum + 1];
+        _newSongs[_currNum] = songInfo;
+        for(int i = 0; i < _bestSongs.Length; i++)
+          _newSongs[i] = _bestSongs[i];
+
+        _bestSongs = _newSongs;
+      }
+      _currNum++;
+    }
+
+
+    public SongInfo[] GetBestSongs()
+    {
+      return _bestSongs;
+    }
+  }
+}
---
+++ DesignPatterns/bin/Debug/DesignPatterns/Behavioural/Iterator/SongsOfThe90s.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.DesignPatterns.Behavioural.Iterator
+{
+  public class SongsOfThe90s
+  {
+
+    private Dictionary<int, SongInfo> _bestSongs;
+
+    private int _currKey = 0;
+
+
+    public SongsOfThe90s()
+    {
+      _bestSongs = new Dictionary<int, SongInfo>();
+
+      AddSong("Losing My Religion", "REM", 1991);
+      AddSong("Creep", "Radiohead", 1993);
+      AddSong("Walk on the Ocean", "Toad the Wet Sprocket", 1991);
+    }
+
+    public void AddSong(string songName, string bandName, int yearRelease)
+    {
+      SongInfo songInfo = new SongInfo(songName, bandName, yearRelease);
+
+      _bestSongs[_currKey] = songInfo;
+
+      _currKey++;
+    }
+
+
+    public Dictionary<int, SongInfo> GetBestSongs()
+    {
+      return _bestSongs;
+    }
+  }
+
+
+}
